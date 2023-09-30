b146e23c7326320d4a4e1d9b1d144f343596f16e
Debasis Mandal <debasismandal900@gmail.com>
2023-10-01 00:33:36 +0530

    initial commit

---
+++ threads/threadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  threadsApp.swift
+//  threads
+//
+//  Created by Debasis Mandal on 01/10/23.
+//
+
+import SwiftUI
+
+@main
+struct threadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
